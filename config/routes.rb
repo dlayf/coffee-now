be833762643b0d2b18ec754deef0af2003d6a8e7
Dan L <dan.layfield@gmail.com>
2015-03-04 19:17:41 -0500

    Adding about and jobs pages'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
 root 'pages#home'
 
+get '/about' => 'pages#about'
+
+get '/jobs' => 'pages#jobs'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
