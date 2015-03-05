305ac67dabb4144b4fba2f255bd685bddd83feac
pekarsky.maxim@gmail.com <flufci@gmail.com>
2015-03-04 20:58:32 -0500

    playing around with google maps api

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,10 +2,26 @@ function main(){
 
 	console.log("ready to go!");
 
+	initialize();
+
+
+
 
 
 
 };
+
+
+  function initialize() {
+        var mapOptions = {
+          center: { lat: 40.7127, lng: -74.0059},
+          zoom: 12
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      
+
 
 
 $(document).ready(main);
